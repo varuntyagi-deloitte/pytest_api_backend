86f1c7c95348d28bac8674024470c1e889fbeb7b
Tyagi <varutyagi@deloitte.com>
2023-05-29 18:36:45 +0530

    This file contains the api calls for three api's and their reponses

---
+++ Mini_Assignment_2/test_url_assignment_2.py
@@ -0,0 +1,44 @@
+import json
+
+import pytest
+import requests
+
+
+@pytest.fixture
+def api_url():
+    return "https://api.instantwebtools.net/v1/airlines/1"
+
+
+@pytest.fixture
+def api_url_second():
+    return "https://api.instantwebtools.net/v1/passenger?page=0&size=10"
+
+
+@pytest.fixture
+def api_url_third():
+    return "https://api.instantwebtools.net/v1/passenger?page=0&size=2"
+
+
+def test_001_get_response(api_url):
+    response = requests.get(f"{api_url}")
+    data = response.json()
+    str_data = json.dumps(data)
+    parsed_data = json.loads(str_data)
+    print(parsed_data)
+
+
+def test_002_get_response(api_url_second):
+    response = requests.get(f"{api_url_second}")
+    data = response.json()
+    str_data = json.dumps(data)
+    parsed_data = json.loads(str_data)
+    print(parsed_data)
+
+
+def test_003_get_response(api_url_third):
+    response = requests.get(f"{api_url_third}")
+    data = response.json()
+    str_data = json.dumps(data)
+    parsed_data = json.loads(str_data)
+    print(parsed_data)
+    print(len(parsed_data["data"]))
